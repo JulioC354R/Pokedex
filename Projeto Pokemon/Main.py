9bfbfcdc0c49fc4558927d8d5fde59769bbacc66
JulioC354R <julyocesarplay@gmail.com>
2023-09-29 01:23:39 -0300

    perfect!!!!!

--- Projeto Pokemon/Main.py
+++ Projeto Pokemon/Main.py
@@ -5,33 +5,49 @@ import requests
 
 print('------- BEM VINDO A POKEDEX -------')
 print('###################################')
-verification = False
-while verification == False:
-    poker_number = int(input('Insira o número do Pokemon desejado...\n'))
-    if poker_number >= 1 and poker_number <= 905:
-        verification = True
-    else:
-        print('Por favor insira um número entre 1 e 905')
+repeat = True
 
-endpoint = f'https://pokeapi.co/api/v2/pokemon/{poker_number}'
-resposta_da_API = requests.get(endpoint)
+while repeat == True:
+    verification = False
+    while verification == False:
+        poker_number = int(input('Insira o número do Pokemon desejado...\n'))
+        if poker_number >= 1 and poker_number <= 905:
+            verification = True
+        else:
+            print('Por favor insira um número entre 1 e 905')
 
-data = resposta_da_API.json()
+    endpoint = f'https://pokeapi.co/api/v2/pokemon/{poker_number}'
+    api_response = requests.get(endpoint)
 
-pokemon = MyPokemon
-name = pokemon.get_nome_pokemon(data)
-image = pokemon.get_image_pokemom(data)
-moves = pokemon.get_golpes_pokemon(data)
-status = pokemon.get_status_pokemon(data)
-types =  pokemon.get_tipos_pokemon(data)
-pokemon_descripion = [name, image, moves, status, types]
+    data = api_response.json()
 
-datas_type = TypePokemon.get_datas_type(data)
+    pokemon = MyPokemon
+    name = pokemon.get_nome_pokemon(data)
+    image = pokemon.get_image_pokemom(data)
+    moves = pokemon.get_golpes_pokemon(data)
+    status = pokemon.get_status_pokemon(data)
+    types =  pokemon.get_tipos_pokemon(data)
+    pokemon_descripion = [name, image, moves, status, types]
 
-pokemon.print_pokemon_description(pokemon_descripion)
+    datas_type = TypePokemon.get_datas_type(data)
 
-i=0
-for damage_list in datas_type:
-    damage_list = TypePokemon.get_damage_list(datas_type[i])
-    TypePokemon.print_damage_list(types[i], damage_list)
-    i += 1
+    pokemon.print_pokemon_description(pokemon_descripion)
+
+    i=0
+    for damage_list in datas_type:
+        damage_list = TypePokemon.get_damage_list(datas_type[i])
+        TypePokemon.print_damage_list(types[i], damage_list)
+        i += 1
+    print('fonte: ', endpoint)
+    loop = True
+    while loop == True:
+        response = input('Deseja pesquisar por outro pokemon??\n S (sim)     ou      N (não)')
+        if response == 's' or response == 'S':
+            repeat = True
+            loop   = False
+        elif response == 'n' or response == 'N':
+            repeat = False
+            loop   = False
+        else:
+            print('Por favor insira uma resposta válida!')
+            loop = True
